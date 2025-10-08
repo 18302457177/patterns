424103541c988e4673b6b63363d7e0c215d6dd24
xxs <x3304845146@gmail.com>
2025-10-08 10:35:04 +0800

    单例模式

--- src/com/xxs/creationalPatterns/singleton/Singleton1.java
+++ src/com/xxs/creationalPatterns/singleton/Singleton1.java
@@ -1,5 +1,8 @@
 package com.xxs.creationalPatterns.singleton;
 
+/**
+ * 饿汉式（静态常量）
+ */
 public class Singleton1 {
     private static final Singleton1 INSTANCE = new Singleton1();
     private Singleton1(){
